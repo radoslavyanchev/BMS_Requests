aeef31e94eec950a53891819d48d6d96f37c2962
Radoslav Yanchev <radoslav.yanchev86@gmail.com>
2016-06-28 17:20:48 +0300

    Class Request

---
+++ src/main/java/com/bms/model/Request.java
@@ -0,0 +1,12 @@
+package com.bms.model;
+
+public class Request {
+	private int id;
+	private Department fromDeparment;
+	private Department forDeparment;
+
+	public Request() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
