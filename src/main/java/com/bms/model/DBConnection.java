3fed7acdc4124c28a38fe08ec6abdfa358f74aee
radoslavyanchev86 <radoslav.yanchev86@gmail.com>
2016-07-27 19:11:01 +0300

    DBConnection

--- src/main/java/com/bms/model/DBConnection.java
+++ src/main/java/com/bms/model/DBConnection.java
@@ -15,7 +15,7 @@ public class DBConnection {
 	private static final String DB_PASSWORD = "";
 	private static final String DB_USER = "root";
 	private static final String DATABASE = "bms_requests";
-	private static final String DB_PORT = "8888";
+	private static final String DB_PORT = "3306";
 	private static final String DB_HOST = "127.0.0.1";
 	private static final String DB_URL = DB_HOST + ":" + DB_PORT;
 
